dc88baddb5572c6c75a6be1621d1302275abb393
Finaritra Mario Rakotonindrina <finarako@c1r1s5.42antananarivo.mg>
2026-04-12 19:43:05 +0300

    fix: remoove trash directory

--- ex6/ft_count_harvest_iterative.py
+++ ex6/ft_count_harvest_iterative.py
@@ -1,3 +1,6 @@
-def  ft_count_harvest_iterative():
-	day = int(input("Days until harvest: "))
-    
+def ft_count_harvest_iterative():
+    day = int(input("Days until harvest: "))
+    day = day + 1
+    display = range(1, day)
+    for n in display:
+        print(f"Day {n}")
--- ex6/ft_count_harvest_recursive.py
+++ ex6/ft_count_harvest_recursive.py
@@ -1,2 +1,7 @@
-def  ft_count_harvest_recursive():
-	
+def ft_count_harvest_recursive(day=None, n=1):
+    if day is None:
+        day = int(input("Days until harvest: "))
+    if n > day:
+        return
+    print(f"Day {n}")
+    ft_count_harvest_recursive(day, n + 1)
